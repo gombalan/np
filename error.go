48d5688e9ddf389b3fb45b5e015f0eb0200ab23a
bagus.brahmantya <bagus.brahmantya@sirclo.com>
2022-09-06 21:38:29 +0700

    try error with stacktrace

--- error.go
+++ error.go
@@ -1,5 +1,11 @@
 package np
 
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
 const (
 	ErrZeroValue        string = "value must be non-zero"
 	ErrNegativeValue    string = "value must be positive"
@@ -7,3 +13,67 @@ const (
 	ErrEmptyArray       string = "array is empty"
 	ErrSizeNotMatch     string = "array size is not match"
 )
+
+func newError(message string, args ...arg) error {
+	return create(nil, message, args)
+}
+
+// func propagate(cause error, message string) error {
+// 	if cause == nil {
+// 		return nil
+// 	}
+// 	return create(cause, message)
+// }
+
+type npError struct {
+	message  string
+	cause    error
+	file     string
+	function string
+	line     int
+}
+
+type arg struct {
+	key   string
+	value interface{}
+}
+
+func create(cause error, message string, args []arg) error {
+	err := &npError{
+		message: fmt.Sprintf("%s: %v", message, args),
+		cause:   cause,
+	}
+
+	pc, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return err
+	}
+
+	err.file, err.line = file, line
+
+	f := runtime.FuncForPC(pc)
+	if f == nil {
+		return err
+	}
+	err.function = shortFuncName(f)
+
+	return err
+}
+
+func shortFuncName(f *runtime.Func) string {
+	longName := f.Name()
+
+	withoutPath := longName[strings.LastIndex(longName, "/")+1:]
+	withoutPackage := withoutPath[strings.Index(withoutPath, ".")+1:]
+
+	shortName := withoutPackage
+	shortName = strings.Replace(shortName, "(", "", 1)
+	shortName = strings.Replace(shortName, "*", "", 1)
+	shortName = strings.Replace(shortName, ")", "", 1)
+
+	return shortName
+}
+
+func (err *npError) Error() string {
+	return fmt.Sprintf("%v", *err)
+}
--- int32ArrayCreation.go
+++ int32ArrayCreation.go
@@ -1,19 +1,17 @@
 package np
 
-import "errors"
-
 func Arange(start int32, stop int32, step int32) Int32OneDArray {
 	if step == 0 {
-		return Int32OneDArray{nil, errors.New(ErrZeroValue)}
+		return Int32OneDArray{nil, newError(ErrZeroValue, arg{key: "step", value: 0})}
 	}
 
 	if start == stop {
-		return Int32OneDArray{nil, errors.New(ErrInvalidParameter)}
+		return Int32OneDArray{nil, newError(ErrInvalidParameter, arg{key: "start", value: start}, arg{key: "stop", value: stop})}
 	}
 
 	size := (stop - start) / step
 	if size < 1 {
-		return Int32OneDArray{nil, errors.New(ErrInvalidParameter)}
+		return Int32OneDArray{nil, newError(ErrInvalidParameter, arg{key: "size", value: size}, arg{key: "step", value: 0}, arg{key: "start", value: start}, arg{key: "stop", value: stop})}
 	}
 
 	result := make([]int32, 0, size)
